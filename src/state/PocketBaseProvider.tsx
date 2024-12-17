bf8fb677c708411b787755ae2886da3062b0d6f8
Billy Pamanian <itisbilly@gmail.com>
2024-12-17 03:55:28 -0800

    Tried to build, PocketBaseProvider.tsx appears to be unused

--- src/state/PocketBaseProvider.tsx
+++ src/state/PocketBaseProvider.tsx
@@ -1,9 +1,9 @@
-import { createContext } from 'react';
-import PocketBase from 'pocketbase';
-
-const url = 'https://billy-blog.pockethost.io/';
-const pb = new PocketBase(url);
-
-const PocketBaseProvider = createContext<any>(null);
-
-export default PocketBaseProvider;
+// import { createContext } from 'react';
+// import PocketBase from 'pocketbase';
+//
+// const url = 'https://billy-blog.pockethost.io/';
+// const pb = new PocketBase(url);
+//
+// const PocketBaseProvider = createContext<any>(null);
+//
+// export default PocketBaseProvider;
